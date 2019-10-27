2d63ee254ff5b9b49486813b152729208332e159
jlramsey <jj@Michaels-iPhone-128217219130.ndc.nasa.gov>
2019-10-27 10:10:34 -0400

    changes

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Node Dependency
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Jswagg2007', // Add your password
+    database : 'burgers_db' // Add your database
+  });
+}
+
+
+// Export the Connection
---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Database Creation
+
+
+USE burgers_db;
+
+
+-- Table Creation
+CREATE TABLE burgers(
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(50),
+devoured BOOLEAN,
+date TIMESTAMP);
